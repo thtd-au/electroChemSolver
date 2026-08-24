317595dc6e2245bee41e84319b3c01eca10eda3a
thtd-au <au672287@uni.au.dk>
2026-08-24 09:17:32 +0200

    updated waterEq to calc A before c clipping

--- applications/solvers/mySolver/electroChem/reactionModels/equilibriumReactions/waterEquilibrium/waterEquilibrium.C
+++ applications/solvers/mySolver/electroChem/reactionModels/equilibriumReactions/waterEquilibrium/waterEquilibrium.C
@@ -15,6 +15,7 @@ waterEquilibrium::waterEquilibrium
     HplusName_(dict.lookup("Hplus")),
     OHminusName_(dict.lookup("OHminus")),
     Kw_(readScalar(dict.lookup("Kw"))),
+    diagA_(false),
     Hplus_
     (
         mesh_.lookupObjectRef<volScalarField>
@@ -53,6 +54,23 @@ waterEquilibrium::waterEquilibrium
             << ": Kw must be greater than zero."
             << exit(FatalIOError);
     }
+
+    const dictionary& controlDict = mesh_.time().controlDict();
+
+    if (controlDict.found("diagnostics"))
+    {
+        const dictionary& diagnosticsDict =
+            controlDict.subDict("diagnostics");
+
+        if (diagnosticsDict.found("waterEquilibrium"))
+        {
+            const dictionary& waterDiagDict =
+                diagnosticsDict.subDict("waterEquilibrium");
+
+            diagA_ =
+                waterDiagDict.getOrDefault<Switch>("diagA", false);
+        }
+    }
 }
 
 
@@ -61,9 +79,15 @@ void waterEquilibrium::apply()
     scalarField& cH = Hplus_.primitiveFieldRef();
     scalarField& cOH = OHminus_.primitiveFieldRef();
 
+    // Capture the transported ion difference before any concentration clipping.
+    // This allows a local over-consumption of H+ or OH- to be supplied by the
+    // instantaneous water equilibrium while conserving A = [H+] - [OH-].
+    scalarField ABefore(cH - cOH);
+
     forAll(cH, celli)
     {
-        const scalar A = cH[celli] - cOH[celli];
+        // const scalar A = cH[celli] - cOH[celli];
+        const scalar A = ABefore[celli];
         const scalar root = sqrt(sqr(A) + 4.0*Kw_);
 
         // /*
@@ -88,9 +112,51 @@ void waterEquilibrium::apply()
         }
     }
 
+    if (diagA_)
+    {
+        const scalarField AAfter(cH - cOH);
+        const scalarField AResidual(AAfter - ABefore);
+
+        const scalar totalVolume = gSum(mesh_.V());
+
+        const scalar ABeforeMin = gMin(ABefore);
+        const scalar ABeforeMean =
+            gSum(ABefore*mesh_.V())/(totalVolume + VSMALL);
+        const scalar ABeforeMax = gMax(ABefore);
+        const scalar ABeforeIntegral = gSum(ABefore*mesh_.V());
+
+        const scalar AAfterMin = gMin(AAfter);
+        const scalar AAfterMean =
+            gSum(AAfter*mesh_.V())/(totalVolume + VSMALL);
+        const scalar AAfterMax = gMax(AAfter);
+        const scalar AAfterIntegral = gSum(AAfter*mesh_.V());
+
+        const scalar maxAResidual = gMax(mag(AResidual));
+        const scalar integralAResidual = gSum(AResidual*mesh_.V());
+
+        Info<< "Water-equilibrium A diagnostics (" << name_ << "):" << nl
+            << "  before min/mean/max = "
+            << ABeforeMin << " / "
+            << ABeforeMean << " / "
+            << ABeforeMax << nl
+            << "  after  min/mean/max = "
+            << AAfterMin << " / "
+            << AAfterMean << " / "
+            << AAfterMax << nl
+            << "  integral before/after = "
+            << ABeforeIntegral << " / "
+            << AAfterIntegral << nl
+            << "  max|A_after-A_before| = "
+            << maxAResidual << nl
+            << "  integral(A_after-A_before) = "
+            << integralAResidual << nl
+            << endl;
+    }
+
     // Internal cells are projected; configured patch conditions remain authoritative.
     Hplus_.correctBoundaryConditions();
     OHminus_.correctBoundaryConditions();
 }
 
 } // End namespace Foam
+
